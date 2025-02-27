62464e3c7e51fb8430c7c4d90f808dc28ce4aca6
Kadir <kdrokszs7@gmail.com>
2025-02-27 20:25:55 +0300

    Güncellemeler yapıldı

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const letters = document.querySelectorAll("h3");
-
-    letters.forEach((letter, index) => {
-        setTimeout(() => {
-            letter.style.color = "white";
-        }, index * 300);
-    });
-});
